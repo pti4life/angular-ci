b9e247434b601ee9e82b5e122dd2e79e27ddd46f
Kallai Roland <roland.kallai@icellmobilsoft.hu>
2021-10-02 15:45:12 +0200

    test fix

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,10 +20,10 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'frontend'`, () => {
+  it(`should have as title 'amazon deploy májsztró'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('frontend');
+    expect(app.title).toEqual('amazon deploy májsztró');
   });
 
   it('should render title', () => {
