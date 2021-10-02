d6b58980971a9641220dbd7b8a6704e8f3b3b18c
Kallai Roland <roland.kallai@icellmobilsoft.hu>
2021-10-02 15:40:26 +0200

    application title added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'frontend';
+  title = 'amazon deploy májsztró';
 
   constructor() {
     
